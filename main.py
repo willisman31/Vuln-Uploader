b8c8a56b4471b8e0b5a182b259a1fd92d0d7a361
Jacob Willis <willisjp@dukes.jmu.edu>
2023-01-11 04:44:37 -0500

    Bug fixes and add return statements

--- main.py
+++ main.py
@@ -6,17 +6,21 @@ DEFAULT_WORDLIST = "./default_wordlist.txt"
 
 def enumerateDirectories(target, wordlist = DEFAULT_WORDLIST):
     word_array=readWordlist(wordlist)
+    discovered_directories=[];
     for (word in word_array):
         request = requests.get(target + "/" + word)
-        if (int) request.status_code >= 200 && request.status_code < 500:
+        if (int) request.status_code >= 200 && request.status_code < 400:
             print(word + " " + request.status_code)
+            discovered_directories.append(word)
+    return discovered_directories
 
 def readWordlist(wordlist=DEFAULT_WORDLIST):
     word_array = []
     file=open(wordlist, "r")
     for line in file:
 	if line[0] != "#" and len(line) >= 1:
-        	word_array.append(line)
+            word_array.append(line)
     file.close()
+    return word_array
 
 
