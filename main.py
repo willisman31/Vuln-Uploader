0a670fdb6a4f780f416484731b3ff3a63f053f01
jacob <willisjp@dukes.jmu.edu>
2023-01-17 15:04:09 -0500

    Fix formatting

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ DEFAULT_WORDLIST = "./default_wordlist.txt"
 def enumerateDirectories(target, wordlist = DEFAULT_WORDLIST):
     word_array=readWordlist(wordlist)
     discovered_directories=[];
-    for (word in word_array):
+    for word in word_array:
         request = requests.get(target + "/" + word)
-        if (int) request.status_code >= 200 && request.status_code < 400:
+        if int(request.status_code) >= 200 and int(request.status_code) < 400:
             print(word + " " + request.status_code)
             discovered_directories.append(word)
     return discovered_directories
@@ -18,7 +18,7 @@ def readWordlist(wordlist=DEFAULT_WORDLIST):
     word_array = []
     file=open(wordlist, "r")
     for line in file:
-	if line[0] != "#" and len(line) >= 1:
+        if line[0] != "#" and len(line) >= 1:
             word_array.append(line)
     file.close()
     return word_array
